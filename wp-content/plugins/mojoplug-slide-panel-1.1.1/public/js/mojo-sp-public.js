7d156a413776387b05b4ca474dcc60ae10118d5a
Mia Jordan <mia@bluetabby.net>
2018-07-09 22:42:58 +0000

    fix slideout - auto or not via widget

--- wp-content/plugins/mojoplug-slide-panel-1.1.1/public/js/mojo-sp-public.js
+++ wp-content/plugins/mojoplug-slide-panel-1.1.1/public/js/mojo-sp-public.js
@@ -566,7 +566,7 @@
     });
 
 	
- setTimeout(function() {
+ 					setTimeout(function() {
                               $('#mojosp-toggle-right').get(0).click();
                             }, 1700);
 	
