5052a192568feff0013d5e08162056b92793cf32
Mia Jordan <mia@bluetabby.net>
2018-08-07 21:22:02 +0000

    change to html bulletin

--- wp-content/themes/Hilltop Theme/single-bulletin.php
+++ wp-content/themes/Hilltop Theme/single-bulletin.php
@@ -232,7 +232,7 @@ td { font-family:'raleway',calibri,arial; font-size:12pt;}
 
 <tr>
 
-                <td style="height:340px;background-image:url(<?php if( !empty($htmlheader) ): ?><?php echo $htmlheader['url']; ?><?php endif; ?>);">
+                <td style="height:340px;background-color:#00a0af;background-image:url(<?php if( !empty($htmlheader) ): ?><?php echo $htmlheader['url']; ?><?php endif; ?>);overflow:hidden;">
 					
 					<table width="680" align="center">
 						<tr>
